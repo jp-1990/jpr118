9d0f304cc479825969acc0eed781029cb155dfd8
jp-1990 <plum_1990@hotmail.co.uk>
2020-12-17 14:17:13 +0000

    create added common date arrays

---
+++ src/helpers/dateFunctions.js
@@ -0,0 +1,23 @@
+export const days = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+]
+export const months = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+]
